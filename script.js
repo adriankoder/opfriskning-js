5e5c789db3c5230c884bcd756a4c60473827ae57
adriankoder <adrian.koder@gmail.com>
2024-06-11 10:56:32 +0200

    test

---
+++ script.js
@@ -0,0 +1,37 @@
+const newelement = document.createElement("p")
+newelement.innerHTML = "i like pizza!";
+newelement.id = "myp"
+document.body.appendChild(newelement)
+console.log(newelement)
+
+
+
+const buttonId = document.getElementById("buttonId");
+buttonId.addEventListener("click", (countSelected) => {
+  const musicTypes = document.getElementById ("musicTypes")  
+  musicTypes
+document.getElementById("hei").textContent = "Denne teksten er endret med javaScript"
+
+// const musicTypes = document.selectForm.musicTypes;
+// console.log(`You have selected ${countSelected(musicTypes)} option(s).`); 
+const javaScriptElement = document.createElement("p")
+javaScriptElement.innerHTML = "nå er knappen trykt";
+javaScriptElement.id = "myp"
+document.body.appendChild(javaScriptElement)
+// document.body.appendChild(hei)
+// document.body.replaceChild(javaScriptElement,hei)
+console.log (javaScriptElement)
+function countSelected(){
+(selectObject)
+for (let j = 0; j < 2; j++) {
+for (let x = 0; x < selectObject.javaScriptElement.length; x++){
+  if (selectObject.javaScriptElement[x].selected){
+    numberSelected++; 
+  }
+}return numberSelected;
+
+}}})
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,75 @@
+const newelement = document.createElement("p")
+newelement.innerHTML = "i like pizza!";
+newelement.id = "myp"
+document.body.appendChild(newelement)
+console.log(newelement)
+
+
+const item = document.getElementById("buttonId");
+    const items = document.querySelectorAll('#myList li');
+
+  items.forEach(function
+    (item, index) {
+item.addEventListener("click", (addTestToArray) => {
+
+  const musicTypes = document.getElementById ("musicTypes")  
+  musicTypes
+// const musicTypes = document.selectForm.musicTypes;
+// console.log(`You have selected ${addElementToArray(musicTypes)} option(s).`); 
+const javaScriptElement = document.createElement("p")
+javaScriptElement.innerHTML = "Denne teksten er endret med javaScript";
+javaScriptElement.id = "myp"
+document.body.appendChild(javaScriptElement)
+console.log (javaScriptElement)
+// function addElementToArray(){
+  const myArray = []
+  function addTestToArray() {
+    myArray.innerHTML = "test";
+    console.log('Nytt element lagt til: ' + element);
+    console.log('Arrayen er nå: ', myArray);
+  }
+    
+    
+          // Sjekk om indeksen er 0 (første element)
+          if (index === 0) {
+              // Legg til "test" i arrayen
+              addTestToArray();
+          }
+      })
+  });// (selectObject)
+// const arr = [1,2,3,4]}
+
+
+
+// for (let i = 0; i< arr.length;i++){
+//   console.log(arr)
+// }
+// arr.forEach(function(elem) {
+// console.log(elem)
+// })
+//   let i = 0;
+//   while(1<arr.length) 
+// console.log(arr[i]);
+// i++
+// }})
+
+
+// // Definere en array
+// var arr = [1, 2, 3, 4, 5];
+
+// // For-løkke for å iterere gjennom arrayen
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// // ForEach-løkke for å iterere gjennom arrayen
+// arr.forEach(function(elem) {
+//     console.log(elem);
+// });
+
+// // While-løkke for å iterere gjennom arrayen
+// var i = 0;
+// while (i < arr.length) {
+//     console.log(arr[i]);
+//     i++;
+// }
