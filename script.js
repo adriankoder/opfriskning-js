40a0c65d378a6ed897f9d6a3bb529f71d41e1ee0
adriankoder <adrian.koder@gmail.com>
2024-06-11 13:08:37 +0200

    oppgave js 2

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
-const newelement = document.createElement("p")
-newelement.innerHTML = "i like pizza!";
-newelement.id = "myp"
-document.body.appendChild(newelement)
-console.log(newelement)
-
-
-
+let condition = true;
+const smileys = ['😀', '😃', '😄', '😁', '😆'];
+ const newelement = document.createElement("p")
+    newelement.innerHTML = "i like pizza!";
+    document.body.appendChild(newelement)
+    console.log(newelement)
 const buttonId = document.getElementById("buttonId");
-buttonId.addEventListener("click", (countSelected) => {
-  // const musicTypes = document.getElementById ("musicTypes")  
-  // musicTypes
-document.getElementById("creat_elementDIVId").textContent = "Denne teksten er endret med javaScript"
 
-// const musicTypes = document.selectForm.musicTypes;
-// console.log(`You have selected ${countSelected(musicTypes)} option(s).`); 
-const javaScriptElement = document.createElement("p")
-javaScriptElement.innerHTML = "nå er knappen trykt";
-javaScriptElement.id = "myp"
-document.body.appendChild(javaScriptElement)
-// document.body.appendChild(hei)
-// document.body.replaceChild(javaScriptElement,hei)
-console.log (javaScriptElement)
-function countSelected(){
-(selectObject)
-for (let j = 0; j < 2; j++) {
-for (let x = 0; x < selectObject.javaScriptElement.length; x++){
-  if (selectObject.javaScriptElement[x].selected){
-    numberSelected++; 
+
+    buttonId.addEventListener("click", function() {
+      
+      // const musicTypes = document.getElementById ("musicTypes")  
+      // musicTypes
+      document.getElementById("creat_elementDIVId").textContent = "Denne teksten er endret med javaScript"
+
+      // const musicTypes = document.selectForm.musicTypes;
+      // console.log(`You have selected ${countSelected(musicTypes)} option(s).`); 
+      const javaScriptElement = document.createElement("p")
+      javaScriptElement.innerHTML = "nå er knappen trykt";
+      document.body.appendChild(javaScriptElement)
+
+
+
+      // document.body.appendChild(hei)
+      // document.body.replaceChild(javaScriptElement,hei)
+      console.log(javaScriptElement);
+      // function countSelected(){
+      // (selectObject)
+      // for (let j = 0; j < 2; j++) {
+      return javaScriptElement;
+    });
+
+ if (condition) {
+  // for (let j = 0; j < 2; j++) {
+      for (let i = 0; i < smileys.length; i++) {
+          let p = document.createElement('p');
+          
+          p.textContent = smileys[i];
+          console.log(p);
+          document.querySelector('#creat_elementDIVId').appendChild(p);
+      }
+  } else {
+    console.log(condition)
+    console.log("Condition not met");
+
+   
+
+
+
+    
+
+
   }
-}return numberSelected;
-
-}}})
-
-
-
-
